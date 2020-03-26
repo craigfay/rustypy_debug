a04d3d7af0bb247a220abc0ed957bfd864e37b5a
craig fay <craigfay1@gmail.com>
2020-03-26 09:21:34 -0500

    Comment python

--- python_code/main.py
+++ python_code/main.py
@@ -1,8 +1,10 @@
 from rustypy.rswrapper import bind_rs_crate_funcs
 
+# These paths will need to change if not running in docker container
 source_path = "/app/rust_code"
 compiled_lib = "/app/rust_code/target/debug/libsome_lib.so"
 lib_binds = bind_rs_crate_funcs(source_path, compiled_lib, prefixes=[])
 
+# Raises Exception!
 print(lib_binds.python_bind_example())
 
